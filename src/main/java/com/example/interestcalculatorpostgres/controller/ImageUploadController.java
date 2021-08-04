98935ecde6e00aac6738af8f006f3d62c77e4e5c
srilekhaballa <80448422+srilekhaballa@users.noreply.github.com>
2021-08-04 12:17:44 +0530

    upload

---
+++ src/main/java/com/example/interestcalculatorpostgres/controller/ImageUploadController.java
@@ -0,0 +1,40 @@
+package com.example.interestcalculatorpostgres.controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping(path = "image") 
+public class ImageUploadController {
+
+	public static String uploadDirectory = System.getProperty("user.dir")+"/uploads";
+
+	@RequestMapping("/")
+	public String UploadPage(Model model) {
+		return "uploadview";
+	} 
+
+	@RequestMapping("/upload")
+	public String upload(Model model,@RequestParam("files") MultipartFile[] files) {
+		StringBuilder fileNames = new StringBuilder();
+		for(MultipartFile file: files) {
+			Path fileNameAndPath= Paths.get(uploadDirectory,file.getOriginalFilename());
+			fileNames.append(file.getOriginalFilename());
+			try {
+				Files.write(fileNameAndPath, file.getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		model.addAttribute("msg","Successfully uploaded files"+fileNames.toString());
+		return "uploadstatusview";
+	}
+
+}
